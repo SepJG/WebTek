a69319ec7ca0c0e51a2cddce799e6b1606937ef5
SepJG <sepantaganjei@gmail.com>
2022-10-29 21:47:51 +0200

    updated a7

--- assignment7/js/todo.js
+++ assignment7/js/todo.js
@@ -1,16 +1,44 @@
-let title = document.getElementById("title");
-let input = document.getElementById("input");
-let div = document.getElementById("div");
-let button = document.getElementById("button");
-let result= document.getElementById("result");
-let list = document.getElementById("myList");
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const result = document.getElementById("result");
+const list = document.getElementById("myList");
+const completedTasks = document.getElementById("completedTasks");
 
+let tasks = [];
+let tasksCompleted = 0;
 
-function addTask(event) {
-  const newList = document.createElement("li");
-  newList.innerText = input.value;
-  list.prepend(newList);
-  event.preventDefault();
+function updateTasks() {
+  completedTasks.innerText = `${tasksCompleted}/${tasks.length} completed`;
 }
 
-button.addEventListener("click", addTask);
+function addTask(event) {
+  event.preventDefault();
+  const newList = document.createElement("li");
+  newList.innerHTML = `
+    <div class="task" name="task">
+      <input type="checkbox" name="checkbox" />
+      <p>${input.value}</p>
+    </div>
+    `;
+  list.prepend(newList);
+  tasks.unshift({ text: input, timestamp: Date.now() });
+  console.log(tasks.length);
+  updateTasks();
+}
+
+button.addEventListener("click", (event) => addTask(event));
+
+list.addEventListener("click", () => {
+  let counter = 0;
+  const tasks = document.querySelectorAll("[name=task]");
+  tasks.forEach((task) => {
+    if (task.firstElementChild.checked) {
+      task.lastElementChild.style.textDecoration = "line-through";
+      counter++;
+    } else {
+      task.lastElementChild.style.textDecoration = "none";
+    }
+  });
+  tasksCompleted = counter;
+  updateTasks();
+});
